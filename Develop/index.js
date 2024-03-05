3a2c2c292858bd86cd354170b738267b7179fef9
Tim Lewis <timothy.james.lewis.31@gmail.com>
2024-03-05 17:13:55 -0600

    It is sucessfully writing a readme from the prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide Installation instructions for your project:',
+    },
+    {
+        type: 'list',
+        message: 'Which license are you using for your project?',
+        name: 'license',
+        choices: ['MIT','Apache-2.0','Mozilla Public License / MPL-2.0', 'None']
+    },
+    {
+        type: 'input',
+        message: 'How can someone contribute to your project',
+        name: 'contributing'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`README.md has been generated successfully as ${fileName}`);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        const readMeContent = generateMarkdown(answers)
+        writeToFile('README.md', readMeContent)
+    })
+}
 
 // Function call to initialize app
 init();
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,90 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    switch (license) {
+      case 'MIT':
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      case 'Apache-2.0':
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      case 'Mozilla Public License / MPL-2.0':
+        return '[![License: MPL-2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      case 'None':
+        return '';
+    }
+   
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    switch (license) {
+      case 'MIT':
+        return '[MIT License](https://opensource.org/licenses/MIT)';
+      case 'Apache-2.0':
+        return '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
+      case 'Mozilla Public License / MPL-2.0':
+        return '[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)';
+      case 'None':
+        return '';
+    }
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == 'None') {
+    return ''
+  } else { 
+    return `## License 
+    This project is licensed under the ${license} License. See the [LICENSE](${renderLicenseLink(license)}) file for details.`}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  WHAT DO I PUT HERE?
+
+  ## Contributing
+
+  ${data.credits}
+
+  ## Tests
+
+  WHAT ARE TESTS IN THIS CASE?
+
+  ## Questions
+
+  ${data.questions}
 `;
 }
 
